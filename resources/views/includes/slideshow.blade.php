e88452b952bc3f08e3d8d7843af36a4473ea6e79
sopheakcarl <37933555+sopheakcarl@users.noreply.github.com>
2018-03-31 13:30:45 +0700

    save cut view

---
+++ resources/views/includes/slideshow.blade.php
@@ -0,0 +1,74 @@
+<div class="headline">
+							<div class="nav" id="headline-nav">
+								<a class="left carousel-control" role="button" data-slide="prev">
+									<span class="ion-ios-arrow-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="right carousel-control" role="button" data-slide="next">
+									<span class="ion-ios-arrow-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
+							</div>
+							<div class="owl-carousel owl-theme" id="headline">							
+								<div class="item">
+									<a href="#"><div class="badge">Tip!</div> Vestibulum ante ipsum primis in faucibus orci</a>
+								</div>
+								<div class="item">
+									<a href="#">Ut rutrum sodales mauris ut suscipit</a>
+								</div>
+							</div>
+						</div>
+						<div class="owl-carousel owl-theme slide" id="featured">
+							<div class="item">
+								<article class="featured">
+									<div class="overlay"></div>
+									<figure>
+										<img src="images/news/img04.jpg" alt="Sample Article">
+									</figure>
+									<div class="details">
+										<div class="category"><a href="category.html">Computer</a></div>
+										<h1><a href="single.html">Phasellus iaculis quam sed est elementum vel ornare ligula venenatis</a></h1>
+										<div class="time">December 26, 2016</div>
+									</div>
+								</article>
+							</div>
+							<div class="item">
+								<article class="featured">
+									<div class="overlay"></div>
+									<figure>
+										<img src="images/news/img14.jpg" alt="Sample Article">
+									</figure>
+									<div class="details">
+										<div class="category"><a href="category.html">Travel</a></div>
+										<h1><a href="single.html">Class aptent taciti sociosqu ad litora torquent per conubia nostra</a></h1>
+										<div class="time">December 10, 2016</div>
+									</div>
+								</article>
+							</div>
+							<div class="item">
+								<article class="featured">
+									<div class="overlay"></div>
+									<figure>
+										<img src="images/news/img13.jpg" alt="Sample Article">
+									</figure>
+									<div class="details">
+										<div class="category"><a href="category.html">International</a></div>
+										<h1><a href="single.html">Maecenas accumsan tortor ut velit pharetra mollis</a></h1>
+										<div class="time">October 12, 2016</div>
+									</div>
+								</article>
+							</div>
+							<div class="item">
+								<article class="featured">
+									<div class="overlay"></div>
+									<figure>
+										<img src="images/news/img05.jpg" alt="Sample Article">
+									</figure>
+									<div class="details">
+										<div class="category"><a href="category.html">Lifestyle</a></div>
+										<h1><a href="single.html">Mauris elementum libero at pharetra auctor Fusce ullamcorper elit</a></h1>
+										<div class="time">November 27, 2016</div>
+									</div>
+								</article>
+							</div>
+						</div>
