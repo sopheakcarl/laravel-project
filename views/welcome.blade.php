78e53dfe137cc54b3cfcbdb7386137026b269d60
sopheakcarl <37933555+sopheakcarl@users.noreply.github.com>
2018-03-31 09:32:24 +0700

    save

--- views/welcome.blade.php
+++ views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel from vin</title>
+        <title>Laravel from sopheak</title>
 
 
         <!-- Fonts -->
