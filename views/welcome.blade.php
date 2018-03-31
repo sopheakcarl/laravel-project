9aa839355c087d299fce419e7c745b40e4b50f77
yonsongheng <mrrrheng@gmail.com>
2018-03-31 16:17:44 +0700

    save

--- views/welcome.blade.php
+++ views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel from sopheak</title>
+        <title>Laravel from heng</title>
 
 
         <!-- Fonts -->
