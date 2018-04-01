f655a00cab3815e9f2249abb878dd9ff24568c33
Chumdavin <chumdavin9@gmail.com>
2018-04-01 13:25:05 +0700

    save

--- resources/views/includes/slideshow.blade.php
+++ resources/views/includes/slideshow.blade.php
@@ -1,75 +1,38 @@
-<div class="headline">
-							<div class="nav" id="headline-nav">
-								<a class="left carousel-control" role="button" data-slide="prev">
-									<span class="ion-ios-arrow-left" aria-hidden="true"></span>
-									<span class="sr-only">Previous</span>
-								</a>
-								<a class="right carousel-control" role="button" data-slide="next">
-									<span class="ion-ios-arrow-right" aria-hidden="true"></span>
-									<span class="sr-only">Next</span>
-								</a>
-							</div>
-							<div class="owl-carousel owl-theme" id="headline">							
-								<div class="item">
-									<a href="#"><div class="badge">Tip!</div>SETEC INSTITUE</a>
-								</div>
-								<div class="item">
-									<a href="#">Ut rutrum sodales mauris ut suscipit</a>
-								</div>
-							</div>
-						</div>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+	<li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
 
-						<div class="owl-carousel owl-theme slide" id="featured">
-							<div class="item">
-								<article class="featured">
-									<div class="overlay"></div>
-									<figure>
-										<img src="images/news/img04.jpg" alt="Sample Article">
-									</figure>
-									<div class="details">
-										<div class="category"><a href="category.html">Computer</a></div>
-										<h1><a href="single.html">Phasellus iaculis quam sed est elementum vel ornare ligula venenatis</a></h1>
-										<div class="time">December 26, 2016</div>
-									</div>
-								</article>
-							</div>
-							<div class="item">
-								<article class="featured">
-									<div class="overlay"></div>
-									<figure>
-										<img src="images/news/img14.jpg" alt="Sample Article">
-									</figure>
-									<div class="details">
-										<div class="category"><a href="category.html">Travel</a></div>
-										<h1><a href="single.html">Class aptent taciti sociosqu ad litora torquent per conubia nostra</a></h1>
-										<div class="time">December 10, 2016</div>
-									</div>
-								</article>
-							</div>
-							<div class="item">
-								<article class="featured">
-									<div class="overlay"></div>
-									<figure>
-										<img src="images/news/img13.jpg" alt="Sample Article">
-									</figure>
-									<div class="details">
-										<div class="category"><a href="category.html">International</a></div>
-										<h1><a href="single.html">Maecenas accumsan tortor ut velit pharetra mollis</a></h1>
-										<div class="time">October 12, 2016</div>
-									</div>
-								</article>
-							</div>
-							<div class="item">
-								<article class="featured">
-									<div class="overlay"></div>
-									<figure>
-										<img src="images/news/img05.jpg" alt="Sample Article">
-									</figure>
-									<div class="details">
-										<div class="category"><a href="category.html">Lifestyle</a></div>
-										<h1><a href="single.html">Mauris elementum libero at pharetra auctor Fusce ullamcorper elit</a></h1>
-										<div class="time">November 27, 2016</div>
-									</div>
-								</article>
-							</div>
-						</div>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/news/img04.jpg" width="100%" hight="100%" alt="SETEC INTITUTE">
+    </div>
+
+    <div class="item">
+      <img src="images/news/img18.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="images/news/img19.jpg" alt="New York">
+    </div>
+
+	<div class="item">
+      <img src="images/news/img20.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
