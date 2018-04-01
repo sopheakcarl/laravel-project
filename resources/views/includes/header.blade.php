89ce9980d5660e47e2374ff4c55b47cddf72eec7
Chumdavin <chumdavin9@gmail.com>
2018-04-01 10:23:38 +0700

    save

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -60,11 +60,11 @@
 							<li class="for-tablet nav-title"><a>Menu</a></li>
 							<li class="for-tablet"><a href="login.html">Login</a></li>
 							<li class="for-tablet"><a href="register.html">Register</a></li>
-							<li><a href="category.html">Home</a></li>
+							<li><a href="category.html">SETEC Intitute</a></li>
 							<li class="dropdown magz-dropdown">
-								<a href="category.html">Pages <i class="ion-ios-arrow-right"></i></a>
+								<a href="category.html">IT Learning <i class="ion-ios-arrow-right"></i></a>
 								<ul class="dropdown-menu">
-									<li><a href="index.html">Home</a></li>
+									<li><a href="index.html">SETEC Intitute</a></li>
 									<li class="dropdown magz-dropdown">
 										<a href="#">Authentication <i class="ion-ios-arrow-right"></i></a>
 										<ul class="dropdown-menu">
@@ -207,54 +207,7 @@
 									</div>
 								</div>
 							</li>
-							<li class="dropdown magz-dropdown magz-dropdown-megamenu"><a href="#">Column <i class="ion-ios-arrow-right"></i></a>
-								<div class="dropdown-menu megamenu">
-									<div class="megamenu-inner">
-										<div class="row">
-											<div class="col-md-3">
-												<h2 class="megamenu-title">Column 1</h2>
-												<ul class="vertical-menu">
-													<li><a href="#">Example 1</a></li>
-													<li><a href="#">Example 2</a></li>
-													<li><a href="#">Example 3</a></li>
-													<li><a href="#">Example 4</a></li>
-													<li><a href="#">Example 5</a></li>
-												</ul>
-											</div>
-											<div class="col-md-3">
-												<h2 class="megamenu-title">Column 2</h2>
-												<ul class="vertical-menu">
-													<li><a href="#">Example 6</a></li>
-													<li><a href="#">Example 7</a></li>
-													<li><a href="#">Example 8</a></li>
-													<li><a href="#">Example 9</a></li>
-													<li><a href="#">Example 10</a></li>
-												</ul>
-											</div>
-											<div class="col-md-3">
-												<h2 class="megamenu-title">Column 3</h2>
-												<ul class="vertical-menu">
-													<li><a href="#">Example 11</a></li>
-													<li><a href="#">Example 12</a></li>
-													<li><a href="#">Example 13</a></li>
-													<li><a href="#">Example 14</a></li>
-													<li><a href="#">Example 15</a></li>
-												</ul>
-											</div>
-											<div class="col-md-3">
-												<h2 class="megamenu-title">Column 4</h2>
-												<ul class="vertical-menu">
-													<li><a href="#">Example 16</a></li>
-													<li><a href="#">Example 17</a></li>
-													<li><a href="#">Example 18</a></li>
-													<li><a href="#">Example 19</a></li>
-													<li><a href="#">Example 20</a></li>
-												</ul>
-											</div>
-										</div>
-									</div>
-								</div>
-							</li>
+							
 							<li class="dropdown magz-dropdown"><a href="#">Dropdown Icons <i class="ion-ios-arrow-right"></i></a>
 								<ul class="dropdown-menu">
 									<li><a href="#"><i class="icon ion-person"></i> My Account</a></li>
